54f41e3a75afb610cb4a19ff611a5edab1996bc3
Branislav Krompić <119957824+banega99@users.noreply.github.com>
2023-04-18 02:34:04 +0200

    vp fix

--- main.js
+++ main.js
@@ -13,6 +13,14 @@ if(level < 5) {
     title.style.marginLeft = '24px'
 }
 
+//Resenje za problem kada tastatura natelefonu pomera sadrzaj stranice na gore
+setTimeout(function () {
+    let viewheight = window.innerHeight;
+    let viewwidth = window.innerWidth;
+    let viewport = document.querySelector("meta[name=viewport]");
+    viewport.setAttribute("content", "height=" + viewheight + "px, width=" + viewwidth + "px, initial-scale=1.0");
+}, 300);
+
 createLevel()
 function createLevel(){ 
     var rowColCon = row.concat(column)
