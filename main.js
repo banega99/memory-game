13e943dc268a7f4de2496b090edeb1a74e91e2e2
Branislav Krompić <119957824+banega99@users.noreply.github.com>
2023-04-18 15:19:26 +0200

    start btn

--- main.js
+++ main.js
@@ -8,125 +8,152 @@ var chsLvl = document.getElementById('chooseLvl')
 var lvlLis = document.getElementsByClassName('lvlLi')
 var logo = document.getElementsByClassName('logo')[0]
 var title = document.getElementById('title')
+var line = document.getElementsByClassName('line')[0]
+var timer = 0
+var timerBtn = 5
+var interval
+var defeat = document.getElementById('defeat')
+if (level == 1) interval = 50
 
-for (let i = 0; i < lvlLis.length; i++) {
-    const lvlLi = lvlLis[i];
-    lvlLi.addEventListener('click', () => {
-        chsLvl.style.display = 'none'
-        level = i+1
-        if(level == 1) {
-            res = 0
-            container.className = 'level1'
-            container.innerHTML = ''
-            var row = ['a', 'b']
-            var column = ['a', 'b']
-            container.innerHTML = ''
-            lvlBtnWdth()
-            var rowColCon = row.concat(column)
-            var rowCol = row.length + column.length
-            rand1 = Math.floor(Math.random() * (rowCol - 1))
-            for (let i = 0; i < rowCol; i++) {
-                var card = document.createElement('div');
-                var backCard = document.createElement('div')
-                var frontCard = document.createElement('div')
-                card.className = 'card'
-                backCard.className = 'back'
-                frontCard.className = 'front'
-                card.appendChild(backCard)
-                card.appendChild(frontCard)
-                container.appendChild(card)
-                var cardsFront = document.getElementsByClassName('front')
-                cardsFront[i].innerHTML = `
-                <img id='imgFront' src="./footb2/${rowColCon[rand1]}.png" alt="" srcset="">
-                <h1>${rowColCon[rand1]}</h1>`
-                rowColCon = rowColCon.toSpliced(rand1, 1)
-                rand1 = Math.floor(Math.random() * (rowColCon.length - 1))
-            }
-        
-            var cards = document.getElementsByClassName('card')
-            var front = document.getElementsByClassName('front')
-            var back = document.getElementsByClassName('back')
-            var compare = []
-            var cardArrF = []
-            var cardArrB = []
-            if (res < row.length) {
-                for (let i = 0; i < cards.length; i++) {
-                    const card = cards[i];
-                    card.addEventListener('click', () => {
-                        front[i].style.transform = 'perspective(900px) rotateY(0deg)'
-                        back[i].style.transform = 'perspective(900px) rotateY(180deg)'
-                        compare.push(front[i].innerText)
-                        cardArrB.push(back[i])
-                        cardArrF.push(front[i])
-                        if (cardArrF.length == 2) {
-                            for (let card of cards) {
-                                card.style.pointerEvents = 'none';
-                            }
-                            setTimeout(() => {
-                                if (compare[0] == compare[1]) {
-                                    for (let card of cards) {
-                                        card.style.pointerEvents = 'all';
-                                    }
-                                    cardArrF[0].parentElement.style.pointerEvents = 'none';
-                                    cardArrF[1].parentElement.style.pointerEvents = 'none';
-                                    res++
-                                    compare = []
-                                    cardArrF = []
-                                    cardArrB = []
-                                    if (res == row.length) {
-                                        compare = []
-                                        cardArrF = []
-                                        cardArrB = []
-                                        level++
-                                        for (let l = 0; l < front.length; l++) {
-                                            const front1 = front[l];
-                                            const back1 = back[l];
-                                            front1.style.transform = 'perspective(900px) rotateY(180deg)'
-                                            back1.style.transform = 'perspective(900px) rotateY(0deg)'
-        
-                                        }
-                                        card.style.pointerEvents = 'all';
-                                        levelSwitch()
-                                    }
-                                } else {
-                                    for (let card of cards) {
-                                        card.style.pointerEvents = 'all';
-                                    }
-                                    for (let k = 0; k < cardArrF.length; k++) {
-                                        cardArrF[k].style.transform = 'perspective(900px) rotateY(180deg)'
-                                        cardArrB[k].style.transform = 'perspective(900px) rotateY(0deg)'
-                                    }
-        
-                                    compare = []
-                                    cardArrF = []
-                                    cardArrB = []
-                                }
-                            }, 500)
-        
-                        } else {
-                            for (let card of cards) {
-                                card.style.pointerEvents = 'all';
-                            }
-                        }
-                    })
-                }
-            }
+// level.btn.
+
+
+// for (let i = 0; i < lvlLis.length; i++) {
+//     const lvlLi = lvlLis[i];
+//     lvlLi.addEventListener('click', () => {
+//         clearInterval(timerLine)
+//         chsLvl.style.display = 'none'
+//         level = i + 1
+//         if (level == 1) {
+//             res = 0
+//             container.className = 'level1'
+//             container.innerHTML = ''
+//             var row = ['a', 'b']
+//             var column = ['a', 'b']
+//             container.innerHTML = ''
+//             lvlBtnWdth()
+//             var rowColCon = row.concat(column)
+//             var rowCol = row.length + column.length
+//             rand1 = Math.floor(Math.random() * (rowCol - 1))
+//             for (let i = 0; i < rowCol; i++) {
+//                 var card = document.createElement('div');
+//                 var backCard = document.createElement('div')
+//                 var frontCard = document.createElement('div')
+//                 card.className = 'card'
+//                 backCard.className = 'back'
+//                 frontCard.className = 'front'
+//                 card.appendChild(backCard)
+//                 card.appendChild(frontCard)
+//                 container.appendChild(card)
+//                 var cardsFront = document.getElementsByClassName('front')
+//                 cardsFront[i].innerHTML = `
+//                 <img id='imgFront' src="./footb2/${rowColCon[rand1]}.png" alt="" srcset="">
+//                 <h1>${rowColCon[rand1]}</h1>`
+//                 rowColCon = rowColCon.toSpliced(rand1, 1)
+//                 rand1 = Math.floor(Math.random() * (rowColCon.length - 1))
+//             }
+
+//             var cards = document.getElementsByClassName('card')
+//             var front = document.getElementsByClassName('front')
+//             var back = document.getElementsByClassName('back')
+//             var compare = []
+//             var cardArrF = []
+//             var cardArrB = []
+//             if (res < row.length) {
+//                 for (let i = 0; i < cards.length; i++) {
+//                     const card = cards[i];
+//                     card.addEventListener('click', () => {
+//                         card.style.pointerEvents = 'none';
+//                         front[i].style.transform = 'perspective(900px) rotateY(0deg)'
+//                         back[i].style.transform = 'perspective(900px) rotateY(180deg)'
+//                         compare.push(front[i].innerText)
+//                         cardArrB.push(back[i])
+//                         cardArrF.push(front[i])
+//                         if (cardArrF.length == 2) {
+//                             for (let card of cards) {
+//                                 card.style.pointerEvents = 'none';
+//                             }
+//                             setTimeout(() => {
+//                                 if (compare[0] == compare[1]) {
+//                                     for (let card of cards) {
+//                                         card.style.pointerEvents = 'all';
+//                                     }
+//                                     cardArrF[0].parentElement.style.pointerEvents = 'none';
+//                                     cardArrF[1].parentElement.style.pointerEvents = 'none';
+//                                     res++
+//                                     compare = []
+//                                     cardArrF = []
+//                                     cardArrB = []
+//                                     if (res == row.length) {
+//                                         compare = []
+//                                         cardArrF = []
+//                                         cardArrB = []
+//                                         level++
+//                                         for (let l = 0; l < front.length; l++) {
+//                                             const front1 = front[l];
+//                                             const back1 = back[l];
+//                                             front1.style.transform = 'perspective(900px) rotateY(180deg)'
+//                                             back1.style.transform = 'perspective(900px) rotateY(0deg)'
+
+//                                         }
+//                                         card.style.pointerEvents = 'all';
+//                                         levelSwitch()
+//                                     }
+//                                 } else {
+//                                     for (let card of cards) {
+//                                         card.style.pointerEvents = 'all';
+//                                     }
+//                                     for (let k = 0; k < cardArrF.length; k++) {
+//                                         cardArrF[k].style.transform = 'perspective(900px) rotateY(180deg)'
+//                                         cardArrB[k].style.transform = 'perspective(900px) rotateY(0deg)'
+//                                     }
+
+//                                     compare = []
+//                                     cardArrF = []
+//                                     cardArrB = []
+//                                 }
+//                             }, 500)
+
+//                         } else {
+//                             for (let card of cards) {
+//                                 card.style.pointerEvents = 'all';
+//                             }
+//                         }
+//                     })
+//                 }
+//             }
+
+//         }
+//         levelSwitch()
+//         lvlBtnWdth()
+//     })
+
+
+// }
+if(levelBtn.innerText === 'START'){
+    levelBtn.addEventListener('click', () => {
+        levelBtn.style.pointerEvents = 'none';
+        // if (chsLvl.style.display == 'none') {
+        //     chsLvl.style.display = 'block'
+        // } else {
+        //     chsLvl.style.display = 'none'
+        // }
+        container.className = 'level1'
+        levelBtn.innerText = timerBtn
+        var timer1 = setInterval(()=>{
             
-        }
-        levelSwitch()
-        lvlBtnWdth()
+            timerBtn --
+            levelBtn.innerText = timerBtn
+            if(timerBtn == 0){
+                levelBtn.style.display = 'none';
+                clearInterval(timer1)
+                createLevel()
+                
+            }
+        }, 1000)
     })
-
-
 }
 
-levelBtn.addEventListener('click', () => {
-    if (chsLvl.style.display == 'none') {
-        chsLvl.style.display = 'block'
-    } else {
-        chsLvl.style.display = 'none'
-    }
-})
 
 lvlBtnWdth()
 
@@ -134,6 +161,7 @@ lvlBtnWdth()
 
 function lvlBtnWdth() {
     if (level < 5 && window.innerWidth < 576) {
+        line.style.top = '60px'
         logo.style.width = '110px'
         logo.style.height = '30px'
         title.style.fontSize = '10px'
@@ -143,6 +171,7 @@ function lvlBtnWdth() {
     }
 
     if (level >= 5 && window.innerWidth < 576) {
+        line.style.top = '40px'
         levelBtn.style.width = '50px'
         chsLvl.style.width = '50px'
         logo.style.width = '70px'
@@ -150,7 +179,7 @@ function lvlBtnWdth() {
         title.style.fontSize = '6px'
         title.style.marginLeft = '14px'
     }
-    
+
 }
 
 //Resenje za problem kada tastatura natelefonu pomera sadrzaj stranice na gore
@@ -161,8 +190,38 @@ function lvlBtnWdth() {
 //     viewport.setAttribute("content", "height=" + viewheight + "px, width=" + viewwidth + "px, initial-scale=1.0");
 // }, 300);
 
-createLevel()
+
+
+// createLevel()
 function createLevel() {
+    console.log(interval)
+    var timerLine = setInterval(() => {
+        timer++
+        console.log(interval)
+        // console.log(timer)
+        line.style.width = `${timer}px`
+        if (line.style.width === `${window.innerWidth}px`) {
+
+            defeat.style.display = 'flex'
+            defeat.addEventListener('click', () => {
+                level = 1
+                res = 0
+                row = ['a', 'b']
+                column = ['a', 'b']
+                timer = 0
+                interval = 50
+                line.style.width = `${timer}px`
+                container.innerHTML = ''
+                levelBtn.style.pointerEvents = 'all';
+                levelBtn.style.display = 'flex'
+                timerBtn = 5
+                levelBtn.innerText = 'START'
+                defeat.style.display = 'none'
+            })
+            clearInterval(timerLine)
+        }
+    }, interval)
+
     container.innerHTML = ''
     lvlBtnWdth()
     var rowColCon = row.concat(column)
@@ -196,17 +255,21 @@ function createLevel() {
         for (let i = 0; i < cards.length; i++) {
             const card = cards[i];
             card.addEventListener('click', () => {
+                card.style.pointerEvents = 'none';
+                // console.log(card)
                 front[i].style.transform = 'perspective(900px) rotateY(0deg)'
                 back[i].style.transform = 'perspective(900px) rotateY(180deg)'
                 compare.push(front[i].innerText)
                 cardArrB.push(back[i])
                 cardArrF.push(front[i])
                 if (cardArrF.length == 2) {
+
                     for (let card of cards) {
                         card.style.pointerEvents = 'none';
                     }
                     setTimeout(() => {
                         if (compare[0] == compare[1]) {
+                            timer = timer - 20
                             for (let card of cards) {
                                 card.style.pointerEvents = 'all';
                             }
@@ -217,10 +280,18 @@ function createLevel() {
                             cardArrF = []
                             cardArrB = []
                             if (res == row.length) {
+                                clearInterval(timerLine)
+                                timer = 0
+                                interval
                                 compare = []
                                 cardArrF = []
                                 cardArrB = []
                                 level++
+                                if(level > 6){
+                                    defeat.style.display = 'block'
+                                    defeat.style.color = 'green'
+                                    defeat.innerText = 'VICTORY'
+                                }
                                 for (let l = 0; l < front.length; l++) {
                                     const front1 = front[l];
                                     const back1 = back[l];
@@ -246,10 +317,6 @@ function createLevel() {
                         }
                     }, 500)
 
-                } else {
-                    for (let card of cards) {
-                        card.style.pointerEvents = 'all';
-                    }
                 }
             })
         }
@@ -262,6 +329,9 @@ function levelSwitch() {
     switch (level) {
         case 2:
             res = 0
+            timer = 0
+            interval = 80
+
             container.className = 'level2'
             container.innerHTML = ''
             row = ['a', 'b', 'c', 'd']
@@ -270,6 +340,9 @@ function levelSwitch() {
             break;
         case 3:
             res = 0
+            timer = 0
+            interval = 150
+
             container.className = 'level3'
             container.innerHTML = ''
             row = ['a', 'b', 'c', 'd', 'e', 'f']
@@ -278,6 +351,9 @@ function levelSwitch() {
             break;
         case 4:
             res = 0
+            timer = 0
+            interval = 220
+
             container.className = 'level4'
             container.innerHTML = ''
             row = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
@@ -286,6 +362,9 @@ function levelSwitch() {
             break;
         case 5:
             res = 0
+            timer = 0
+            interval = 300
+
             container.className = 'level5'
             container.innerHTML = ''
             row = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l']
@@ -294,6 +373,8 @@ function levelSwitch() {
             break;
         case 6:
             res = 0
+            timer = 0
+            interval = 380
             container.className = 'level6'
             container.innerHTML = ''
             row = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r']
